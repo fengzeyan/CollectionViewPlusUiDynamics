220da5949620fe772bb52fcb7d52aae5b1fa8c9d
Rui Marinho <rui@mbprui.home>
2014-04-16 15:00:20 +0100

    Initial commit
    Xamarin port of the following article:
    http://www.objc.io/issue-5/collection-views-and-uidynamics.html

---
+++ CollectionViewPlusUiDynamics/CollectionViewPlusUiDynamicsViewController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.Collections.Generic;
+
+namespace CollectionViewPlusUiDynamics
+{
+	public partial class CollectionViewPlusUiDynamicsViewController : UIViewController
+	{
+		public CollectionViewPlusUiDynamicsViewController (IntPtr handle) : base (handle)
+		{
+		}
+		static NSString gridCellId = new NSString ("GridViewCell");
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		UICollectionView collectionView;
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+
+			base.ViewDidLoad ();
+
+			// Perform any additional setup after loading the view, typically from a nib.
+			collectionView = new UICollectionView (View.Bounds,new SpringyCollectionViewFlowLayout());
+		
+			collectionView.BackgroundColor = UIColor.Red;
+			collectionView.DataSource = new TableSource ();
+			collectionView.RegisterClassForCell (typeof(GridViewItemCell),gridCellId);
+				this.Add (collectionView);
+		
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			collectionView.CollectionViewLayout.InvalidateLayout ();
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+
+
+		public class TableSource : UICollectionViewDataSource
+		{
+			static NSString gridCellId = new NSString ("GridViewCell");
+
+			Random rand = new Random (1);
+			public List<string> items = new List<string> ();
+			public TableSource ()
+			{
+				for (int i = 0; i < 1000; i++) {
+
+					items.Add ( "hello " + i);
+				}
+
+			}
+
+
+			#region implemented abstract members of UICollectionViewDataSource
+			public override int GetItemsCount (UICollectionView collectionView, int section)
+			{
+				return items.Count;
+			}
+			public override UICollectionViewCell GetCell (UICollectionView collectionView, NSIndexPath indexPath)
+			{
+				var cell = (GridViewItemCell)collectionView.DequeueReusableCell (gridCellId, indexPath);
+
+				var item = items [indexPath.Row];
+				cell.Text = item;
+
+				return cell;
+			}
+			#endregion
+
+
+		}
+	}
+
+	public class GridViewItemCell : UICollectionViewCell
+	{
+
+		public override void Draw (System.Drawing.RectangleF rect)
+		{
+			base.Draw (rect);
+		}
+		UILabel txtView;
+
+		[Export ("initWithFrame:")]
+		public GridViewItemCell (System.Drawing.RectangleF frame) : base (frame)
+		{
+			this.AutoresizingMask = UIViewAutoresizing.All;
+			this.ContentMode = UIViewContentMode.ScaleAspectFill;
+
+		
+			txtView = new UILabel (new RectangleF(10,10,300,30));
+			txtView.TextColor = UIColor.White;
+			txtView.Font = UIFont.FromName("Helvetica-Bold", 20f);
+
+
+			ContentView.AddSubview (txtView);
+		}
+
+		public string Text {
+			set {
+				txtView.Text = value;
+			}
+		}
+
+	
+	}
+}
+
---
+++ CollectionViewPlusUiDynamics/SpringyCollectionViewFlowLayout.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using System.Linq;
+using MonoTouch.Foundation;
+
+namespace CollectionViewPlusUiDynamics
+{
+	public class SpringyCollectionViewFlowLayout : UICollectionViewFlowLayout
+	{
+		UIDynamicAnimator dynamicAnimator;
+		int margin = 10;
+		float latestDelta;
+		public SpringyCollectionViewFlowLayout ()
+		{
+			this.MinimumInteritemSpacing = margin;
+
+			this.MinimumLineSpacing = margin;
+			this.ItemSize = new SizeF(100,100);
+			dynamicAnimator = new UIDynamicAnimator (this);
+		}
+		public override void InvalidateLayout ()
+		{
+			base.InvalidateLayout ();
+		}
+		public override void PrepareLayout ()
+		{
+
+
+			base.PrepareLayout ();
+			if (this.dynamicAnimator.Behaviors.Count () == 0) {
+				var s = this.CollectionViewContentSize;
+				var rec = new RectangleF (0, 0, s.Width, s.Height);
+				foreach (var item in base.LayoutAttributesForElementsInRect(rec)) {
+					UIAttachmentBehavior behaviour = new UIAttachmentBehavior (item, item.Center);
+					behaviour.Length = 0.0f;
+					behaviour.Damping = 0.8f;
+					behaviour.Frequency = 1.0f;
+					dynamicAnimator.AddBehavior (behaviour);
+				}
+			}
+		}
+		public override bool ShouldInvalidateLayoutForBoundsChange (RectangleF newBounds)
+		{
+						var scrollView = this.CollectionView;
+						var delta = newBounds.X - scrollView.Bounds.Y;
+			
+						var touchLocation = this.CollectionView.PanGestureRecognizer.LocationInView (this.CollectionView);
+							//[self.collectionView.panGestureRecognizer locationInView:self.collectionView];
+						foreach (UIAttachmentBehavior springBehaviour in this.dynamicAnimator.Behaviors) {
+							var yDistanceFromTouch = Math.Abs(touchLocation.Y - springBehaviour.AnchorPoint.Y);
+							var xDistanceFromTouch = Math.Abs(touchLocation.X - springBehaviour.AnchorPoint.X);
+							var scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 1500.0f;
+			
+							var item = springBehaviour.Items.First();
+							var center = item.Center;
+							if (delta < 0)
+								center.X += Math.Max (delta, delta * scrollResistance);
+							else
+								center.X += Math.Min (delta, delta * scrollResistance);
+			
+							item.Center = center;
+							this.dynamicAnimator.UpdateItemUsingCurrentState (item);
+							latestDelta = delta;
+						}
+			return false;
+
+		}
+
+		public override UICollectionViewLayoutAttributes LayoutAttributesForItem (NSIndexPath path)
+		{
+				return this.dynamicAnimator.GetLayoutAttributesForCell (path);
+
+		}
+
+
+
+		public override UICollectionViewLayoutAttributes[] LayoutAttributesForElementsInRect (RectangleF rect)
+		{
+				return dynamicAnimator.GetDynamicItems (rect).Select(c=> 
+				{
+					return (c as UICollectionViewLayoutAttributes);
+				}
+			).ToArray();
+		}
+	}
+}
+
